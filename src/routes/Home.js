200d65c30ac9a8b5abdb9e7e22d870f022aff474
MunSoJeong <sgh04056@naver.com>
2023-02-02 00:39:34 +0900

    🎨feat : 기존 코드 수정 및 검색 추가

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,12 +1,12 @@
 import { useState, useEffect, useMemo } from "react";
-import Card from '../components/Card';
+import AmiiboCard from '../components/AmiiboCard';
 
 function Home() {
     const [amiibo, setAmiibo] = useState([]);
     const [search, setSearch] = useState('');
-    
+
     const getAmiibo = async () => {
-        const res = await(
+        const res = await (
             await fetch('https://www.amiiboapi.com/api/amiibo/?amiiboSeries=Animal%20Crossing&type=card')
         ).json();
         setAmiibo(res.amiibo)
@@ -14,29 +14,28 @@ function Home() {
     useEffect(() => {
         getAmiibo();
     }, []);
-    
+
     const filterAmiibo = useMemo(() => {
         return amiibo.filter((update) => update.release.jp === '2015-07-30')
     }, [amiibo])
 
-    const searching = (e) => {
-        e.preventDefault();
-        // console.log(e.target.value);
+    const userInput = (e) => {
         setSearch(e.target.value)
-    }
-
+    };
+    const searching = filterAmiibo.filter((el) => {
+        return el.name.match(new RegExp(userInput, 'gi'))
+    });
+    
     return (
         <div className="main">
-            <div className="cont">
-                <form>
-                    <input type='text' placeholder='검색어를 입력해주세요.' value={ search } onChange={ searching } />
-                    <button type='submit'>search</button>
-                </form>
+            <div className="contents">
+                <input type='text' placeholder='검색어를 입력해주세요.' value={search} onChange={userInput} />
                 {filterAmiibo.map((v) => (
-                    <Card card={v} />
+                    <AmiiboCard key={v.tail} {...v} />
                 ))}
             </div>
         </div>
     )
 };
+
 export default Home;
