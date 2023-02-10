021312ca2e8daab16f594090e182808a43d53896
MunSoJeong <sgh04056@naver.com>
2023-02-10 13:30:59 +0900

    🗑️cleanup : /.yarn 삭제

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     container: { center: true },
     extend: {
       // 기존 스타일에 추가로 값을 넣어줄 경우에는 extend 객체 안에 쓰기
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
     },
     screens: {
       sm: '465px',
